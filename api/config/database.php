c723fee1f9251c66ced53d5dd9b1b751166234d1
epulido9812 <48631027+epulido9812@users.noreply.github.com>
2019-03-30 16:29:56 -0500

    Update database.php

--- api/config/database.php
+++ api/config/database.php
@@ -2,7 +2,7 @@
 class Database{
  
     // specify your own database credentials
-    private $host = "localhost";
+    private $host = "mysql";
     private $db_name = "PHPLearning";
     private $username = "root";
     private $password = "toor";
